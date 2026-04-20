c7aa694813c67afabb69cefff3a0a69eebe52c72
StephanieStargirl <>
2026-04-20 11:10:49 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,11 @@
+# DiscountVariable
+discountEligible = True
+
+# Total cost of Grocery bill
+total_cost = 25.00
+
+#Discount check
+discountEligible = total_cost >= 20.00
+
+# Print the result
+print("Is the purchase eligible for a discount?", discountEligible)
